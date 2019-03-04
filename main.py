06e636f94f60f85722b8d8d2e4a85a5bc993c6e3
forbesjon2 <forbesjon2@gmail.com>
2019-03-03 22:03:24 -0600

    main.py changes. Changed time.sleep to subprocess sleep

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import Modules
-import re
 import psycopg2
-import time
-
+import subprocess
 conn = psycopg2.connect(host="localhost",database="ditto", user="localrole", password="Noderink1")
-
-Modules.Transcribe.updateScript(conn)
-# conn.close()
-Modules.Transcribe.resetScript(conn, 12)
 minsPassed = 0
 while(True):
-    Modules.Transcribe.runAutoCheck(conn, 12)
-    time.sleep(60)
-    minsPassed += 1
-    # run reset script every 8 hours
-    if(minsPassed % 480 == 0):
-        Modules.Transcribe.resetScript(conn, 12)
+    Modules.Transcribe.runAutoCheck(conn, 18)
+    subprocess.call("sleep 60s", shell=True)
 
 # conn.close()
 
